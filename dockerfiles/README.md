5516b2a927c28b059bfb7dff90f625933c45ccde
FlorianWoelki <FlorianWoelki@gmx.de>
2022-01-10 15:17:52 +0100

    chore: improve output

--- dockerfiles/README.md
+++ dockerfiles/README.md
@@ -1,3 +1,11 @@
 # Dockerfiles for Kira
 
 This directory contains custom images and dockerfiles for custom code execution.
+
+The sub directories always contain three files:
+
+|File|Description|
+|---|---|
+|`Dockerfile`|Describes the to be build docker image.|
+|`build.sh`|Builds the docker image and uses the local `Dockerfile`.|
+|`deploy.sh`|Builds and deploys the TypeScript docker image.
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
 	"time"
 
@@ -130,9 +131,10 @@ func execute(runner *sandbox.Runner, code string) {
 		panic(err)
 	}
 
-	for _, op := range output {
-		fmt.Println(op)
-	}
+	fmt.Println("\n=== BUILD OUTPUT ===")
+	fmt.Printf("Error: %s, Body: %s\n\n", strconv.FormatBool(output[0].Error), output[0].Body)
+	fmt.Println("=== RUN OUTPUT ===")
+	fmt.Printf("Error: %s, Body: %s\n", strconv.FormatBool(output[1].Error), output[1].Body)
 
 	stopTicking <- true
 }
