4c6d20d5c823cf45a6e6dd297d78c690bf2a896d
FlorianWoelki <FlorianWoelki@gmx.de>
2022-05-04 10:26:53 +0200

    feat(frontend): add basic ability to send request to REST api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,32 @@ import { MonacoEditor } from './Editor';
 const App: React.FC = (): JSX.Element => {
   const codeEditorRef = useRef<any>(null);
 
+  const runCode = async (): Promise<void> => {
+    const result = await fetch('http://localhost:9090/execute', {
+      method: 'POST',
+      body: JSON.stringify({
+        language: 'python',
+        content: codeEditorRef.current.getValue(),
+      }),
+    });
+
+    const jsonResult = await result.json();
+    console.log(jsonResult);
+  };
+
   return (
-    <div className="antialiased h-screen">
+    <div className="relative antialiased h-screen">
       <MonacoEditor
         value="print('Hello World')"
         ref={codeEditorRef}
       ></MonacoEditor>
+
+      <button
+        className="absolute right-0 bottom-0 px-4 py-2 bg-green-600 rounded text-white mb-4 mr-4 transition duration-100 ease-in-out hover:bg-green-500"
+        onClick={runCode}
+      >
+        Run
+      </button>
     </div>
   );
 };
