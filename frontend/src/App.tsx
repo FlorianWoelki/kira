146056f997dc5936ea0475d2afaaf37d60c765f2
FlorianWoelki <FlorianWoelki@gmx.de>
2022-10-09 11:33:03 +0200

    feat(frontend): add button loading animator

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,14 @@ const App: React.FC = (): JSX.Element => {
   const codeEditorRef = useRef<any>(null);
   const [codeResult, setCodeResult] = useState<string>('');
 
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   const runCode = async (): Promise<void> => {
+    if (isLoading) {
+      return;
+    }
+
+    setIsLoading(true);
     const result = await fetch('http://localhost:9090/execute', {
       method: 'POST',
       body: JSON.stringify({
@@ -27,6 +34,7 @@ const App: React.FC = (): JSX.Element => {
     }
 
     console.log(jsonResult);
+    setIsLoading(false);
   };
 
   return (
@@ -39,10 +47,37 @@ const App: React.FC = (): JSX.Element => {
         <p>Output: {codeResult.length === 0 ? '/' : codeResult}</p>
       </div>
       <button
-        className="absolute right-0 bottom-0 px-4 py-2 bg-green-600 rounded text-white mb-4 mr-4 transition duration-100 ease-in-out hover:bg-green-500"
+        className="flex items-center absolute right-0 bottom-0 px-4 py-2 bg-green-600 rounded text-white mb-4 mr-4 transition duration-100 ease-in-out hover:bg-green-500 disabled:cursor-not-allowed disabled:bg-opacity-50"
         onClick={runCode}
+        disabled={isLoading}
       >
-        Run
+        {isLoading ? (
+          <>
+            <svg
+              className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+            <span>Running...</span>
+          </>
+        ) : (
+          <span>Run</span>
+        )}
       </button>
     </div>
   );
