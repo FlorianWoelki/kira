6214928409bd06679ea94459758e27ec887d98d0
FlorianWoelki <FlorianWoelki@gmx.de>
2022-10-12 08:34:31 +0200

    feat(frontend): add Checkbox component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { useRef, useState } from 'react';
+import { Checkbox } from './Checkbox';
 import { MonacoEditor } from './Editor';
 
 interface CodeExecutionResult {
@@ -46,39 +47,43 @@ const App: React.FC = (): JSX.Element => {
       <div className="absolute left-0 bottom-0 px-4 py-2 mb-4 ml-4 bg-gray-200 rounded">
         <p>Output: {codeResult.length === 0 ? '/' : codeResult}</p>
       </div>
-      <button
-        className="flex items-center absolute right-0 bottom-0 px-4 py-2 bg-green-600 rounded text-white mb-4 mr-4 transition duration-100 ease-in-out hover:bg-green-500 disabled:cursor-not-allowed disabled:bg-opacity-50"
-        onClick={runCode}
-        disabled={isLoading}
-      >
-        {isLoading ? (
-          <>
-            <svg
-              className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"
-              ></circle>
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              ></path>
-            </svg>
-            <span>Running...</span>
-          </>
-        ) : (
-          <span>Run</span>
-        )}
-      </button>
+
+      <div className="absolute right-0 bottom-0 mb-4 mr-4 flex items-center space-x-4">
+        <Checkbox id="bypass-checkbox">Bypass Cache</Checkbox>
+        <button
+          className="flex items-center px-4 py-2 bg-green-600 rounded text-white transition duration-100 ease-in-out hover:bg-green-500 disabled:cursor-not-allowed disabled:bg-opacity-50"
+          onClick={runCode}
+          disabled={isLoading}
+        >
+          {isLoading ? (
+            <>
+              <svg
+                className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+              <span>Running...</span>
+            </>
+          ) : (
+            <span>Run</span>
+          )}
+        </button>
+      </div>
     </div>
   );
 };
---
+++ frontend/src/Checkbox.tsx
@@ -0,0 +1,24 @@
+interface Props {
+  id: string;
+  children?: React.ReactNode | React.ReactNode[];
+}
+
+export const Checkbox: React.FC<Props> = (props): JSX.Element => {
+  return (
+    <div className="relative flex items-start">
+      <div className="flex h-5 items-center">
+        <input
+          id={props.id}
+          type="checkbox"
+          name={props.id}
+          className="w-4 h-4 rounded border-gray-300 text-green-600 focus:ring-green-600"
+        />
+      </div>
+      <div className="ml-2 text-sm">
+        <label htmlFor={props.id} className="font-medium text-gray-700">
+          {props.children}
+        </label>
+      </div>
+    </div>
+  );
+};
