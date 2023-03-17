a81786776a097b068ae19a96dd65fa3b570db6cb
FlorianWoelki <FlorianWoelki@gmx.de>
2023-03-17 10:38:21 +0100

    refactor(languages/python): move from unshare to nice

--- languages/python/run.sh
+++ languages/python/run.sh
@@ -1,1 +1,2 @@
-runuser -l "$1" -c -- "unshare -r timeout -s KILL 3 blocksyscalls python3 $2"
+# runuser -l "$1" -c -- "unshare -r timeout -s KILL 3 blocksyscalls python3 $2"
+runuser -l "$1" -c -- "nice timeout -s KILL 3 prlimit --nproc=64 --nofile=2048 --fsize=10000000 blocksyscalls python3 $2"
