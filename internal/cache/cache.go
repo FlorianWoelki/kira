1331c158f1f7adaf7fb2b785747cd36e146af30c
FlorianWoelki <FlorianWoelki@gmx.de>
2023-04-05 08:30:29 +0200

    docs(internal/cache): add comments to cache implementation

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -8,10 +8,14 @@ import (
 	"github.com/go-redis/cache/v8"
 )
 
+// Cache is a generic cache implementation that can store and retrieve values of any type.
 type Cache[T any] struct {
+	// internal represents the internally redis cache.
 	internal *cache.Cache
 }
 
+// NewCache creates a new cache instance and returns a pointer to it. It also creates a
+// new local cache from redis which is a TinyLFU cache.
 func NewCache[T any]() *Cache[T] {
 	return &Cache[T]{internal: cache.New(&cache.Options{
 		Redis:      redisClient,
@@ -19,7 +23,10 @@ func NewCache[T any]() *Cache[T] {
 	})}
 }
 
+// Set stores a value in the cache for a given language and content key. The TTL is one
+// hour. It will also return an error, if an error occured while inserting the value.
 func (c *Cache[T]) Set(language, content string, value T) error {
+	// Create one key by encoding language and content.
 	key := encodeContent(language + content)
 	if err := c.internal.Set(&cache.Item{
 		Ctx:   context.Background(),
@@ -33,7 +40,10 @@ func (c *Cache[T]) Set(language, content string, value T) error {
 	return nil
 }
 
+// Get retrieves a value from the cache for a given language and content key. It will
+// return an error, if an error occured while getting the key or value.
 func (c Cache[T]) Get(language, content string) (T, error) {
+	// Encode content to get one key.
 	key := encodeContent(language + content)
 
 	var wanted T
@@ -44,15 +54,7 @@ func (c Cache[T]) Get(language, content string) (T, error) {
 	return wanted, nil
 }
 
+// encodeContent encodes a string into a base64 string.
 func encodeContent(content string) string {
 	return base64.StdEncoding.EncodeToString([]byte(content))
-}
-
-func decodeHash(hash string) (string, error) {
-	s, err := base64.StdEncoding.DecodeString(hash)
-	if err != nil {
-		return "", err
-	}
-
-	return string(s), nil
 }
--- internal/cache/redis.go
+++ internal/cache/redis.go
@@ -2,6 +2,7 @@ package cache
 
 import "github.com/go-redis/redis/v8"
 
+// redisClient is an internally used cache and connected to port `6379`.
 var redisClient = redis.NewClient(&redis.Options{
 	Addr:     "cache:6379",
 	DB:       0,
